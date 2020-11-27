e83a31081c5f80cb9d938e21b1714d11be72a6ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-27 09:34:12 +0000

    Deploy BlueBrain/CoreNeuron to github.com/BlueBrain/CoreNeuron.git:gh-pages

---
+++ doxygen/navtreedata.js
@@ -0,0 +1,58 @@
+var NAVTREE =
+[
+  [ "CoreNEURON", "index.html", [
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"abort_8cpp.html",
+"classcoreneuron_1_1_file_handler.html#a8b2dd64180912e6af72bb7811c08a53e",
+"classcoreneuron_1_1_t_queue.html#afa422e29d5d1e62172aca863c1e07458",
+"file__utils_8hpp.html",
+"mem__layout__util_8cpp.html#aca02285b777730e95b7368dc52c13b80",
+"namespacemembers_func_n.html",
+"nrn__setup_8cpp.html#a265a70e0aceff0f06813fb815e21fea4",
+"nrnran123_8h.html#a8406d05fde4b30519725ec31fdb2f3d4",
+"register__mech_8hpp_source.html",
+"structcoreneuron_1_1_inter_thread_event.html",
+"structcoreneuron_1_1_void_func.html#a6ea376e58fb79178d03cd06babbfb1ea"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ doxygen/report__configuration__parser_8cpp.js
@@ -0,0 +1,15 @@
+var report__configuration__parser_8cpp =
+[
+    [ "TargetType", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5", [
+      [ "Compartment", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5a75e94a804e5a3c64ba2ebb65701529e3", null ],
+      [ "Soma", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5a06bf2406b68bb5e334936b786ed0ef92", null ],
+      [ "Axon", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5a8f7ac21c5122e81484a165ed24871aca", null ],
+      [ "Dendrite", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5a8570a10ff6f05a313e02c2fbeb0ce2f2", null ],
+      [ "Apical", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5ad3b6e39a9db9b35bd74dceed83cefda0", null ],
+      [ "AxonComp", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5a0aeb6d2d41fa9d13f1b698d4d993f932", null ],
+      [ "DendriteComp", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5a3101b35376d9d834d4c3c19498f136a9", null ],
+      [ "ApicalComp", "report__configuration__parser_8cpp.html#a21fac5edfe2cd8ab9b9698d243b01ea5a16f49b66453f467fa0f9128ae3d006a6", null ]
+    ] ],
+    [ "create_report_configurations", "report__configuration__parser_8cpp.html#a2c1e85d57d6fafefe59d1468d5cb95b5", null ],
+    [ "parse_filter_string", "report__configuration__parser_8cpp.html#a0c36fff87c7eebe3dc8c98d584935df5", null ]
+];
